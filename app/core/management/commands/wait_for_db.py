9523245fbf58d741a58097dd9bf174bc9fdd75f0
juliusMalmstrom <mrmalmstrom1@gmail.com>
2023-03-09 15:34:41 +0100

    fixed linting issues related to test_commands and wait_for_db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -8,6 +8,7 @@ from psycopg2 import OperationalError as Psycopg2Error
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Django command for wait for db."""
 
@@ -15,13 +16,11 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        
         while db_up is False:
             try:
-                self.check(database=['default'])
+                self.check(databases=['default'])
                 db_up = True
             except (Psycopg2Error, OperationalError):
                 self.stdout.write('database unavailable')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS("Database available!"))
-
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,8 +10,9 @@ from unittest.mock import patch
 from psycopg2 import OperationalError as Psycopg2Error
 
 from django.core.management import call_command
-from django.db.utils import OperationalError    
+from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTest(SimpleTestCase):
@@ -26,7 +27,8 @@ class CommandTest(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operational error."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * \
+            2 + [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
